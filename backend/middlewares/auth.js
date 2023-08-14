48c2050f4d0fc150d3b3e6ffcf6e9b3f1bd62642
Pashkova <malkusha@mail.ru>
2023-08-14 11:11:45 +0300

    first commit

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { UnauthorizedError } = require('../errors/index');
+
+const extractBearerToken = (header) => header.replace('Bearer ', '');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    return next(err);
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+  return next(); // пропускаем запрос дальше
+};
