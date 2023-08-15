b2114e98e6b40c4f18b167013cad91d4317774cc
Pashkova <malkusha@mail.ru>
2023-08-16 01:56:04 +0300

    infoToolTip

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,20 +1,30 @@
 import * as auth from '../utils/auth';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
+import InfoToolTip from './InfoTooltip';
+import Success from '../images/Success.svg';
+import Fail from '../images/Fail.svg';
+
 
 function Login({handleLogin}) {
 
   const navigate = useNavigate();
+  const [isFailToolTipOpen, setIsFailToolTipOpen] = useState(false);
+
+  function handleFailToolTip() {
+    setIsFailToolTipOpen(true);
+  }
 
   function handleAuthorize({email, password}) {
     auth.authorize(email, password)
       .then((data) => {
-          console.log(data);
           localStorage.setItem("token", data.token);
           navigate("/", {replace: true});
           handleLogin(email);
         })
       .catch((err) => {
+        handleFailToolTip();
         console.log(`Ошибка: ${err}`)
       });
   }
@@ -27,6 +37,13 @@ function Login({handleLogin}) {
         submitTitle="Войти"
         onSubmit={handleAuthorize}
       />
+    <InfoToolTip
+      popupName="fail"
+      title={<><p className="popup__text">Что-то пошло не так!</p><p className="popup__text">Попробуйте ещё раз.</p></>} 
+      toolTipImg={Fail}
+      isOpen={isFailToolTipOpen}
+      onClose={() => setIsFailToolTipOpen(false)}
+    />
     </div>
   )
 }
